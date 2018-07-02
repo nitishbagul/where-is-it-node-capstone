0b4d420bb7497fc016eecab707bffc9dad97c528
Nitish Bagul <nitishbagul@Nitishs-MacBook-Pro.local>
2018-07-01 20:05:13 -0700

    started with jquery

--- public/client.js
+++ public/client.js
@@ -1,3 +1,60 @@
+//Step 1: Define functions, objects and variables
+
+
+
+
+//Step 2: Use functions, objects and variables(Triggers)
+//when the page loads...
+$(document).ready(function () {
+    $('main').hide();
+    $('.log-in-container').hide();
+    $('.register-container').hide();
+    $('.welcome-page').show();
+
+});
+
+//button triggers
+$(document).on('click', '.show-login-container', function (event) {
+    event.preventDefault();
+    // alert("hi");
+    $('main').hide();
+    $('.register-container').hide();
+    $('.log-in-container').show();
+    $('.welcome-page').show();
+});
+
+$(document).on('click', '.show-registration-container', function (event) {
+    event.preventDefault();
+    // alert("hi");
+    $('main').hide();
+    $('.log-in-container').hide();
+    $('.register-container').show();
+    $('.welcome-page').show();
+});
+
+$(document).on('click', '.logout-button', function (event) {
+    event.preventDefault();
+    location.reload();
+});
+
+
+//form trigger
+$(document).submit('.login-form', function (event) {
+    event.preventDefault();
+    alert("hi");
+    $('main').hide();
+    $('.items-page').show();
+
+});
+
+
+
+
+
+
+
+
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
